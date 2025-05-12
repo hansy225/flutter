f80e1ed93c42de0bf54a8535362fec998ba9c49e
HanSuyeon <sy02251105@gmail.com>
2025-05-12 11:53:58 +0900

    0512

--- dart_basic/bin/02_type_dynamic.dart
+++ dart_basic/bin/02_type_dynamic.dart
@@ -1,3 +1,15 @@
+/*
+  - var 타입 : 처음 들어온 값이 type이 됨. 처음 타입이 정해지면 바꿀 수 없음.
+  - dynamic 타입 :  처음 들어온 값이 type이 됨. 처음 type과 상관없이 타입을 바꿀 수 있음.
+ */
+
 void main () {
+  dynamic name = 'kim';
+  print(name.runtimeType);
+
+  name = 5;
+  print(name.runtimeType);
   
+  var name2 = 'ji';
+  // name2 = 2;  오류
 }
